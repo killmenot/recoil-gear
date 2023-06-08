d7f31e098d3977744ff60f9bd65b50045a12ef89
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2023-06-08 19:21:52 +0300

    chore: bump recoil to 0.7.7

--- apps/example/src/app/app.spec.tsx
+++ apps/example/src/app/app.spec.tsx
@@ -1,17 +1,26 @@
 import { render } from '@testing-library/react';
+import { RecoilRoot } from 'recoil'
 
 import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    );
 
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
+    const { getByText } = render(
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    );
 
-    expect(getByText('Welcome to example!')).toBeTruthy();
+    expect(getByText('Recoil connector to Redux DevTools')).toBeTruthy();
   });
 });
